092a56537c33afdfbb0958e5c143c2babf86458b
tdoganay <tjdoganay@gmail.com>
2023-12-04 13:57:38 -0500

    Created Login Form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Form from "./Form"
+ 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Home Page
-        </p>
-      </header>
-    </div>
+      return (
+        <div className="App">
+              <Form />
+        </div>
+     
   );
 }
-
+ 
 export default App;
