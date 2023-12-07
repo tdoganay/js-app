8a8d816d598d9cffb25444f26bb3f43720784470
tdoganay <tjdoganay@gmail.com>
2023-12-07 18:21:53 -0500

    Removed react-cookie from packages. Implemented js-cookie for cookie management. Made Navbar dynamic based on user state

--- src/App.js
+++ src/App.js
@@ -4,40 +4,37 @@ import Signup from "./Signup"
 import Login from "./Login"
 import Navbar from "./Navbar"
 import {Routes, Route, Navigate, useNavigate} from 'react-router-dom'
-import { CookiesProvider, useCookies } from "react-cookie"
+import Cookies from 'js-cookie';
  
 function App() {
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   const navigate = useNavigate();
 
   function handleLogin(user) {
     console.log(user);
-    setCookie("user", user, { path: "/" });
+    Cookies.set("user", user);
     navigate("/");
   }
 
   function handleLogout() {
-    removeCookie("user", { path: "/" });
+    Cookies.remove("user");
     navigate("/");
   }
 
   function handleSignup(user) {
-    setCookie("user", user, { path: "/" });
+    Cookies.set("user", user);
     navigate("/");
   }
 
   return (
-    <CookiesProvider>
-      <div>
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+      <>
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <Navbar onLogout={handleLogout}/>
         <Routes>
-            <Route path='/' element={cookies.user ? <Home /> : <Navigate to="/login" />}></Route>
-            <Route path='/login' element={cookies.user ? <Navigate to="/" /> : <Login onLogin = {handleLogin} />}></Route>
-            <Route path='/signup' element={cookies.user ? <Navigate to="/" /> :<Signup onSignup = {handleSignup} />}></Route>
+            <Route path='/' element={Cookies.get('user') ? <Home /> : <Navigate to="/login" />}></Route>
+            <Route path='/login' element={Cookies.get('user') ? <Navigate to="/" /> : <Login onLogin = {handleLogin} />}></Route>
+            <Route path='/signup' element={Cookies.get('user') ? <Navigate to="/" /> :<Signup onSignup = {handleSignup} />}></Route>
         </Routes>
-      </div>
-    </CookiesProvider>
+      </>
   );
 }
  
